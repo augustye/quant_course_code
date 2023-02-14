f6ac2e8093c67807b3c22c41c2694e19150b0343
August Ye <augustye@gmail.com>
2023-02-14 17:30:36 +0800

    update class11 strategy

--- vnpy/strategies/class_11_simple_strategy.py
+++ vnpy/strategies/class_11_simple_strategy.py
@@ -14,8 +14,8 @@ class Class11SimpleStrategy(CtaTemplate):
         self.bg5 = BarGenerator(self.on_bar, 5, self.on_5min_bar, Interval.MINUTE)
         self.bg_1hour = BarGenerator(self.on_bar, 1, self.on_1hour_bar, Interval.HOUR)
 
-        #self.place_order = False
-        #self.orders = []
+        self.place_order = False
+        self.orders = []
         # self.pos #
 
     def on_init(self):
@@ -24,7 +24,6 @@ class Class11SimpleStrategy(CtaTemplate):
         """
         self.write_log("策略初始化")
 
-
     def on_start(self):
         """
         Callback when strategy is started.
@@ -32,41 +31,25 @@ class Class11SimpleStrategy(CtaTemplate):
         self.write_log("策略启动")
         self.put_event()
 
-
     def on_stop(self):
         """
         Callback when strategy is stopped.
         """
         self.write_log("策略停止")
-
         self.put_event()
 
-
     def on_tick(self, tick: TickData):
-
-        print(f"tick, ask1:{tick.ask_price_1}, {tick.ask_volume_1}, bid:{tick.bid_price_1}, {tick.bid_volume_1}")
-
-        '''
-        print(f"my current pos is: {self.pos}, ask:{tick.ask_price_1}, bid: {tick.bid_price_1}")
-
+        print(f"tick, ask1:{tick.ask_price_1}, {tick.ask_volume_1}, bid:{tick.bid_price_1}, {tick.bid_volume_1}, pos: {self.pos}")
         if self.place_order is False and self.trading:
-            buy_order = self.buy(Decimal(tick.bid_price_1 * 0.9999), Decimal("0.5"))
-            sell_order = self.sell(Decimal(tick.ask_price_1 * 1.0002), Decimal("0.5"))
-
-            # self.short()
-            # self.cover()  #
-
-            # self.buy()
-            # self.short()
             self.place_order = True
-            print(f"buy_order: {buy_order}, sell_order: {sell_order}")
+            buy_order = self.buy(tick.bid_price_1 * 0.9999, 0.01)
+            sell_order = self.sell(tick.ask_price_1 * 1.0001, 0.01)
+            # self.short()
+            # self.cover() 
+            print(f"placed buy_order: {buy_order}, sell_order: {sell_order}")
             self.orders.extend(buy_order)
             self.orders.extend(sell_order)
 
-        self.bg2.update_tick(tick)
-        self.bg5.update_tick(tick)
-        self.bg_1hour.update_tick(tick)
-        '''
         self.bg.update_tick(tick)
 
     def on_bar(self, bar: BarData):
@@ -94,31 +77,21 @@ class Class11SimpleStrategy(CtaTemplate):
         self.put_event()
 
     def on_1hour_bar(self, bar:BarData):
-
         print("1小时的K线数据", bar)
         self.put_event()
 
     def on_order(self, order: OrderData):
-        """
-        订单的回调方法: 订单状态更新的时候，会调用这个方法。
-        """
-
-        print("策略推送过来的order: ", order)
-
+        print("订单状态更新:", order)
         self.put_event()
-
-
 
     def on_trade(self, trade: TradeData):
         """
         订单成交的推送，比如你下10个BTC,那么可能不会一下子成交，会不断慢慢的成交，
         这时有成交它就会推送给你，告诉你成交了多少，还有多少没有成交
         系统通过里面处理这个方法，知道你当前的仓位数量
-
         """
         print("最新的成交: ", trade)
         self.put_event()  # 更新UI界面方法。
-
 
     def on_stop_order(self, stop_order: StopOrder):
         """
